178f4c943d996b9acfee1fb4526b50491bebdb45
Fer Catalano <fernando@hazak.tech>
2025-03-24 19:03:53 -0300

    Add - new banner tiktokers

---
+++ inc/parts/banner-tiktokers.php
@@ -0,0 +1,17 @@
+<div class="banner-tiktokers mt-0 mb-0">
+    <div class="container">
+        <div class="row">
+            <div class="banner-shopping__item col-lg-3 col-md-6 col-xs-12">
+                <img src="img/config/5be196_new_logo.webp" width="180" alt="logo">
+            </div>
+            <div class="banner-shopping__item col-lg-6 col-md-6 col-xs-12">
+                <i class="fa fa-shopping-cart fa-3x"></i>
+                <h4 class="banner-shopping__title">LÍNEA TIKTOKERS</h4>
+                <p class="banner-shopping__text">Lo nuevo para chic@s y jovenes!</p>
+            </div>
+            <div class="banner-shopping__item col-lg-3 col-md-6 col-xs-12">
+                <img src="img/config/5be196_new_logo.webp" width="180" alt="logo">
+            </div>
+        </div>
+    </div>
+</div>
