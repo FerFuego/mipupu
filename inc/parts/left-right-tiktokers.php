72d7b815f054f426cfff80a56485659d8c20bcb5
Fer Catalano <fernando@hazak.tech>
2025-03-29 16:40:29 -0300

    Add -  style home

--- inc/parts/left-right-tiktokers.php
+++ inc/parts/left-right-tiktokers.php
@@ -4,12 +4,14 @@
             <a href="/productos.php?id_rubro=4">
                 <img class="img-fluid" src="img/ads/ads-01.webp" alt="">
                 <h2>Niñas y Niños</h2>
+                <div class="left-right-section__button">VER PRODUCTOS</div>
             </a>
         </div>
         <div class="col-lg-6 px-0 left-right-section__ads animate__animated animate__fadeInRight animate__delay-1s">
             <a href="/productos.php?id_rubro=68">
                 <img class="img-fluid" src="img/ads/ads-02.webp" style="height: 100% !important;" alt="">
                 <h2>Adolecentes</h2>
+                <div class="left-right-section__button">VER PRODUCTOS</div>
             </a>
         </div>
     </div>
