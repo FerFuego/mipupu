c947ddf69ab8e7a7014bb615977ada31e33cea6f
Fer Catalano <fernando@hazak.tech>
2025-03-29 16:32:57 -0300

    Add -  style home

--- inc/parts/left-right-mipupu.php
+++ inc/parts/left-right-mipupu.php
@@ -4,14 +4,12 @@
             <a href="/productos.php?id_rubro=58">
                 <img class="img-fluid" src="img/ads/ads-2.webp" alt="">
                 <h2>Recién Nacidos</h2>
-                <div class="left-right-section__button">VER PRODUCTOS</div>
             </a>
         </div>
         <div class="col-lg-6 px-0 left-right-section__ads animate__animated animate__fadeInRight animate__delay-1s">
             <a href="/productos.php?id_rubro=18">
                 <img class="img-fluid" src="img/ads/ads-1.webp" style="height: 100% !important;" alt="">
                 <h2>Bebés</h2>
-                <div class="left-right-section__button">VER PRODUCTOS</div>
             </a>
         </div>
     </div>
