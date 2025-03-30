461bba0f9546648a52dac7829a658166c26412e6
Fer Catalano <fernando@hazak.tech>
2025-03-30 10:59:07 -0300

    Add - fix related products

--- inc/parts/related-products.php
+++ inc/parts/related-products.php
@@ -1,21 +1,24 @@
 <section class="related-product">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="section-title related__product__title">
-                    <h2>Productos Relacionados</h2>
+<?php   
+    $products = new Productos();
+    $results = $related->getRelatedProducts($product->getRubroID(), $product->getSubRubroID(), $product->getGrupoID(), $product->getID());
+    
+    if ( $results->num_rows > 0 ) : ?>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title related__product__title">
+                        <h2>Productos Relacionados</h2>
+                    </div>
                 </div>
             </div>
+            <div class="row owl-carousel">
+                <?php while ( $product = $results->fetch_object() ) : ?>
+                    <?php require 'inc/partials/product-card.php'; ?>
+                <?php endwhile; ?>
+            </div>
         </div>
-        <div class="row">
-            <?php 
-                $related = new Productos();
-                $results = $related->getRelatedProducts($product->getRubroID(), $product->getSubRubroID(), $product->getGrupoID(), $product->getID());
-
-                while ( $product = $results->fetch_object() ) :
-                    require 'inc/partials/product-card.php';
-                endwhile; 
-            ?>
-        </div>
-    </div>
+        
+    <?php endif; ?>
 </section>
