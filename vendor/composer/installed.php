60906d8f619a14c5713aa58945aa5b0ebc4dfd8f
Fer Catalano <fernando@hazak.tech>
2025-11-27 18:23:36 -0300

    Add - MP SDK

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '14296a8cae202e161aaacd12120a8db1b5c313ae',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '14296a8cae202e161aaacd12120a8db1b5c313ae',
+            'dev_requirement' => false,
+        ),
+        'mercadopago/dx-php' => array(
+            'pretty_version' => '3.7.1',
+            'version' => '3.7.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mercadopago/dx-php',
+            'aliases' => array(),
+            'reference' => 'ca1c9b495b6b5ba75bed92652110b1ad9ea834c6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
